158e1aa556af3dc2ddc00153b9ce486efe19f18f
lloydnoone <lloydnoone92@gmail.com>
2020-01-24 12:57:34 +0000

    2hours

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('connected!')
+window.addEventListener('DOMContentLoaded', (e) => {
+  const num = document.querySelector('#number')
+  window.addEventListener('click', (e) => num.innerHTML = Math.floor(Math.random() * 28))
+})
+
+//TODO
+//figure out circle progress bar
+//add bar random rezising functionality
+// final touches on fonts and margins
